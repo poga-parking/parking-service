301a50b7e033272fcf6b184f26c8482d94de40f0
Eduard Zaripov <edikgoose@email.com>
2023-12-08 08:59:06 +0300

    feat: fix swagger for HTTPS

--- src/main/kotlin/poga/parking/parkingservice/ParkingServiceApplication.kt
+++ src/main/kotlin/poga/parking/parkingservice/ParkingServiceApplication.kt
@@ -1,14 +1,14 @@
 package poga.parking.parkingservice
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import poga.parking.parkingservice.configuration.properties.PriceListProperties
 
 @SpringBootApplication
-@OpenAPIDefinition
+@OpenAPIDefinition(servers = [Server(url = "https://api.pogaparking.ru", description = "Default Server URL")])
 @ConfigurationPropertiesScan(basePackageClasses = [PriceListProperties::class])
 class ParkingServiceApplication
 
